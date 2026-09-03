8f1c7416553555ce549d2125522d73185aaac9d3
Xin Jin <Xin.C.Jin@noaa.gov>
2026-09-03 14:52:38 +0000

    minor

--- aardvark/ocelot_vendor/obs_config_urma.py
+++ aardvark/ocelot_vendor/obs_config_urma.py
@@ -1,8 +1,4 @@
 """
-Vendored from ocelot3's ocelot/obs_config_urma.py -- see ocelot_vendor/__init__.py.
-Only the import path changed (ocelot.obs_config_utils -> ocelot_vendor.obs_config_utils).
-
-Original docstring:
 Observation configuration module - Python equivalent of observation_config.yaml
 
 This module provides the same configuration data as observation_config.yaml
@@ -38,7 +34,7 @@ OBSERVATION_CONFIG = {
     "state": {
         "ges": {
             'source': 'zarr',
-            'features': ['tmp_2maboveground', 'pres_surface', 'vgrd_10maboveground', 'ugrd_10maboveground', 'dpt_2maboveground',] ,
+            'features': ['tmp_2maboveground', 'pres_surface', 'vgrd_10maboveground', 'ugrd_10maboveground', 'spfh_2maboveground',] ,
             # 'features': ['tmp_2maboveground', 'hgt_surface', 'pres_surface', 'vgrd_10maboveground',  'ugrd_10maboveground',
             #              'dpt_2maboveground',] ,
             "target_dim": 5,              # number of state variables
@@ -52,146 +48,38 @@ OBSERVATION_CONFIG = {
     'conventional': {
         'diag_t': {
             'source': 'zarr',
-            'zarr_name': 'diag_urma_t',
-            'features': ['observation', 'pressure', 'omb'],
-            'metadata': ['height'],
-            'dropna_cols': ['height'],
-            'drop_rows': {
-                'analysis_use_flag': [-1]
-            },
-            'input_dim': 12,
-            'target_dim': 2,
+            'zarr_name': 'surface_obs_t',
+            'features': ['t'],
             'encoder_hidden_layers': 2,
             'decoder_hidden_layers': 2,
-            'qc_filters': {
-                'height': {
-                    'range': [-357, 6000],
-
-                },
-                'observation': {
-                    'range': [228.15, 312.15],
-
-                },
-                'pressure': {
-                    'range': [5.56, 7.09],
-
-                },
-                'omb': {
-                    'range': [-15., 15.],
-
-                }
-            },
-            'subsample': {
-                'mode': 'random',
-                'fraction': 1,
-                'seed': 12345
-            }
         },
-        'diag_uv': {
+        'diag_u': {
             'source': 'zarr',
-            'zarr_name': 'diag_urma_uv',
-            'features': ['observation', 'pressure', 'omb'],
-            'metadata': ['height'],
-            'dropna_cols': ['height'],
-            'drop_rows': {
-                'analysis_use_flag': [-1]
-            },
+            'zarr_name': 'surface_obs_u',
+            'features': ['u'],
             'encoder_hidden_layers': 2,
             'decoder_hidden_layers': 2,
-            'qc_filters': {
-                'height': {
-                    'range': [-64, 5776],
-
-                },
-                'observation': {
-                    'range': [-25, 44],
-
-                },
-                'pressure': {
-                    'range': [5.56, 7.09],
-
-                },
-                'omb': {
-                    'range': [-25., 44],
-                }
-            },
-            'subsample': {
-                'mode': 'random',
-                'fraction': 1,
-                'seed': 12345
-            }
+        },
+        'diag_v': {
+            'source': 'zarr',
+            'zarr_name': 'surface_obs_v',
+            'features': ['v'],
+            'encoder_hidden_layers': 2,
+            'decoder_hidden_layers': 2,
         },
         'diag_ps': {
             'source': 'zarr',
-            'zarr_name': 'diag_urma_ps',
-            'features': ['observation', 'pressure', 'omb'],
-            'metadata': ['height'],
-            'dropna_cols': ['height'],
-            'drop_rows': {
-                'analysis_use_flag': [-1]
-            },
+            'zarr_name': 'surface_obs_ps',
+            'features': ['ps'],
             'encoder_hidden_layers': 2,
             'decoder_hidden_layers': 2,
-            'qc_filters': {
-                'height': {
-                    'range': [-67, 5612],
-
-                },
-                'observation': {
-                    'range': [500, 1052],
-
-                },
-                'pressure': {
-                    'range': [5.56, 7.09],
-
-                },
-                'omb': {
-                    'range': [-500., 500.],
-
-                },
-
-            },
-            'subsample': {
-                'mode': 'random',
-                'fraction': 1,
-                'seed': 12345
-            }
         },
          'diag_q': {
             'source': 'zarr',
-            'zarr_name': 'diag_urma_q',
-            'features': ['observation', 'pressure', 'omb'],
-            'metadata': ['height'],
-            'dropna_cols': ['height'],
-            'drop_rows': {
-                'analysis_use_flag': [-1]
-            },
+            'zarr_name': 'surface_obs_q',
+            'features': ['q'],
             'encoder_hidden_layers': 2,
             'decoder_hidden_layers': 2,
-            'qc_filters': {
-                'height': {
-                    'range': [-357, 6000],
-
-                },
-                'observation': {
-                    'range': [0, 30],
-
-                },
-                'pressure': {
-                    'range': [5.56, 6.97],
-
-                },
-                'omb': {
-                    'range': [-17., 64.],
-
-                },
-
-            },
-            'subsample': {
-                'mode': 'random',
-                'fraction': 1,
-                'seed': 12345
-            }
         },
     }
 }
@@ -201,66 +89,54 @@ apply_derived_instrument_dims(OBSERVATION_CONFIG)
 # Feature statistics
 FEATURE_STATS = {
     'diag_t': {
-        'observation': [276.58, 10.81],
-        'height': [573.49, 696.63],
-        'pressure': [6.89, 0.08],
-        'omb': [0.00, 2.66]
+        't': [289.11, 11.93],
     },
      'diag_q': {
-        'observation': [4.18, 3.04],
-        'height': [543.96, 648],
-        'pressure': [6.89, 0.08],
-        'omb': [0.05, 0.92]
+        'q': [0.01, 0.01],
     },
 
      'diag_ps': {
-        'observation': [978, 55],
-        'height': [349.41, 488.67],
-        'pressure': [6.89, 0.08],
-        'omb': [-1.27, 15.5],
+        'ps': [ 95853.9, 3856.44],
     },
 
-    'diag_uv': {
-        'observation': [0.3, 2.12],
-        'height': [640, 695],
-        'pressure': [6.89, 0.08],
-        'omb': [-0.3, 1.93],
-
+    'diag_u': {
+        'u': [-0.07, 2.54],
+   
+    },
+    'diag_v': {
+        'u': [-0.81, 4.06],
+   
     },
 
     'ges': {
-        'hgt_surface': [442, 622],
-        'pres_surface': [96000,  6950],
-        'vgrd_10maboveground': [0.10, 5.0],
-        'ugrd_10maboveground': [1.2, 4.5],
-        'dpt_2maboveground': [273.15, 13.5],
-        'tmp_2maboveground': [280.14, 13.0],
+        'pres_surface': [95020.7, 4063.36],
+        'vgrd_10maboveground': [0.82, 4.87],
+        'ugrd_10maboveground': [0.05, 2.61], 
+        'spfh_2maboveground': [0.01, 0.01],
+        'tmp_2maboveground': [289, 11.95], 
     }
 }
 
 STATE_INCREMENT_STATS = {
     "tmp_2maboveground": [
-        0.00416477081149185,
-        0.06005789850646087
+        0.0034647618349895526,
+        0.07001476349932656
     ],
     "pres_surface": [
-        0.000574698606485636,
-        0.006262742829476267
+        1.077697040534138,
+        4.34615488592226
     ],
     "vgrd_10maboveground": [
-        -0.0011717678905649221,
-        0.15212414347233272
+        -0.00799981470978102,
+        0.083828175611821
     ],
     "ugrd_10maboveground": [
-        -0.010390013714132994,
-        0.1720638438077753
+        -0.006471472003871081,
+        0.07079058308031667
     ],
-    "hgt_surface": [
-        0.0,
-        1
-    ],
-    "dpt_2maboveground": [
-        -0.0008408632737459072,
-        0.06351003475323037
+    "spfh_2maboveground": [
+        1.5229471766891314e-05,
+        4.344852107939282e-05
     ]
 }
+
